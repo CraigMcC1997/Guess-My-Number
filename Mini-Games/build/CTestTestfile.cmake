af428fe8373387957b379d1e9137f57a66133895
Craig McCorrisken <craigmccorrisken@gmail.com>
2023-03-01 15:09:47 +0000

    Started set up for automated testing using GoogleTest Added initial readme file for testing
    Signed-off-by: Craig McCorrisken <craigmccorrisken@gmail.com>

--- Mini-Games/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: N:/Code Repositories/Testing Adding Build System and Testing/Mini-Games
-# Build directory: N:/Code Repositories/Testing Adding Build System and Testing/Mini-Games/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("N:/Code Repositories/Testing Adding Build System and Testing/Mini-Games/build/hello_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
